e7e97ebf0ac89e81698cf3aed801b52a3e45c2e9
Andrew Tam <andrew.tam.000@gmail.com>
2018-06-23 02:08:13 -0400

    Added reducer for sortable method

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,7 +6,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { connect } from 'react-redux';
 import { sortedRepositorySelector, relevantIssuesSelector } from '../../providers';
-import { changeRepository, asyncUpdateIssues } from '../../redux/actions';
+import { changeRepository, asyncUpdateIssues, rearrangeIssues } from '../../redux/actions';
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
 
 const ListItemWithRepoChange = connect(
     null,
@@ -18,6 +19,16 @@ const ListItemWithRepoChange = connect(
     })
 )(ListItem);
 
+const SortableIssue = SortableElement( props => <ListItem {...props}/>);
+const SortableIssueList = SortableContainer( ({repoName, ...props}) => <List {...props}/>);
+
+const SortableIssueListWithUpdate = connect(
+    null,
+    (dispatch, props) => ({
+        onSortEnd: ({oldIndex, newIndex}) => dispatch(rearrangeIssues(props.repoName, oldIndex, newIndex))
+    })
+)(SortableIssueList);
+
 class Dashboard extends Component {
 
     componentDidMount() {
@@ -27,47 +38,47 @@ class Dashboard extends Component {
     }
 
     render() {
-        const { repositories, issues } = this.props;
+        const { repositories, issues, repoName} = this.props;
 
         const gridWidth = issues.length ? 6 : 12;
+
+        const repositoryRows = _.map(
+            repositories,
+            repo => {
+                const { name, id, created_at: createdAt } = repo;
+                const owner = _.get(repo, ['owner', 'login']);
+                return (
+                    <ListItemWithRepoChange button key={id} id={id} owner={owner} name={name}>
+                        <ListItemText primary={name} secondary={createdAt} />
+                    </ListItemWithRepoChange>
+                )
+            }
+        );
+
+        const issueRows = _.map(
+            issues,
+            (issue, index) => {
+                const { title, updated_at: updatedAt, created_at: createdAt, id } = issue;
+                return (
+                    <SortableIssue button key={id} index={index}>
+                        <ListItemText primary={title} secondary={createdAt} />
+                    </SortableIssue>
+                )
+            }
+        );
 
         return (
             <Grid container>
                 <Grid item xs={gridWidth}>
                     <List>
-                        {
-                            _.map(
-                                repositories,
-                                repo => {
-                                    const { name, id, created_at: createdAt } = repo;
-                                    const owner = _.get(repo, ['owner', 'login']);
-                                    return (
-                                        <ListItemWithRepoChange button key={id} id={id} owner={owner} name={name}>
-                                            <ListItemText primary={name} secondary={createdAt} />
-                                        </ListItemWithRepoChange>
-                                    )
-                                }
-                            )
-                        }
+                        { repositoryRows }
                     </List>
                 </Grid>
                 {
                     issues.length ? (
-                        <List>
-                            {
-                                _.map(
-                                    issues,
-                                    issue => {
-                                        const { title, updated_at: updatedAt, created_at: createdAt, id } = issue;
-                                        return (
-                                            <ListItem button key={id}>
-                                                <ListItemText primary={title} secondary={createdAt} />
-                                            </ListItem>
-                                        )
-                                    }
-                                )
-                            }
-                        </List>
+                        <SortableIssueListWithUpdate repoName={repoName}>
+                            { issueRows }
+                        </SortableIssueListWithUpdate>
                     ) : (
                         null
                     )
@@ -79,7 +90,11 @@ class Dashboard extends Component {
 
 export default connect(
     state => {
+        const selectedRepository = _.get(state, ['general', 'selectedRepository']);
+        const repoName = _.get(state, ['repositories', selectedRepository, 'name']);
+
         return {
+            repoName,
             issues: relevantIssuesSelector(state),
             repositories: sortedRepositorySelector(state)
         }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -21,9 +21,29 @@ export default combineReducers({
     },
     issues: (state = {}, {payload, type}) => {
         switch(type) {
+            case 'REARRANGE_ISSUES':
+                const wantedIssues = state[payload.repoName];
+                const { oldIndex, newIndex }  = payload;
+
+                if (oldIndex == newIndex) {
+                    return state;
+                }
+
+                const rearrangedIssues = newIndex > oldIndex ? [
+                    ...wantedIssues.slice(0, oldIndex),
+                    ...wantedIssues.slice(oldIndex + 1, newIndex + 1),
+                    wantedIssues[oldIndex],
+                    ...wantedIssues.slice(newIndex + 1)
+                ] : [
+                    ...wantedIssues.slice(0, newIndex),
+                    wantedIssues[oldIndex],
+                    ...wantedIssues.slice(newIndex, oldIndex),
+                    ...wantedIssues.slice(oldIndex + 1)
+                ];
+
+                return _.assign({}, state, { [payload.repoName]: rearrangedIssues});
             case 'UPDATE_ISSUES':
-                const { repoName, issues } = payload;
-                return _.assign({}, state, { [repoName]: issues});
+                return _.assign({}, state, { [payload.repoName]: payload.issues});
             default:
                 return state;
         }
