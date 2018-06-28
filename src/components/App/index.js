183b41ce24b183a85f3b0b8a3f27614249781d3b
Andrew Tam <andrew.tam.000@gmail.com>
2018-06-27 23:43:43 -0400

    Moving things into proper file hierarchy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import './App.css';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import store from './redux/store';
+import store from '../../redux/store';
 import { Provider } from 'react-redux';
 
-import Login from './components/Login/';
-import Dashboard from './components/Dashboard/';
+import Login from '../Login/';
+import Dashboard from '../Dashboard/';
 
 class App extends Component {
 
